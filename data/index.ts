fea0d9f07fb3dd752cd7af495716d32f8722ea55
shivangbirla <birlashivang01@gmail.com>
2024-09-29 00:32:29 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "EventSutra – Your pass to every event!",
+    des: "Explore a modern events app with sleek UI, live pricing, and seamless ticketing for users and admins.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://bookmyevent-liard.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "BigWigMedia – AI-powered content made easy!",
+    des: "Transform your task management experience with GPT-4 at BigWigMedia.ai for seamless efficiency.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://bigwigmedia.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Plant Disease Detection",
+    des: "Detect plant diseases effortlessly with our web app, featuring a 15+ layered CNN that achieves 98.3% accuracy!",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://plant-disease-detection-346209.web.app/",
+  },
+  {
+    id: 4,
+    title: "NFT Marketplace",
+    des: "Effortlessly trade ERC-721 tokens with our marketplace app, complete with secure ERC-20 transfers and Metamask support!",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://timelessnft-33dea.web.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Shivang has been an absolute game-changer for our project. His ability to understand complex requirements and deliver robust, scalable solutions is unmatched. He’s a true professional who goes above and beyond to ensure the success of every project he takes on.",
+    name: "Shashwat Mehrotra",
+    title: "Director of Constituents AI",
+  },
+  {
+    quote:
+      "Shivang's expertise as a full-stack developer was crucial to the success of our app. He handled both frontend and backend challenges with ease and delivered a seamless user experience. He’s proactive, communicates clearly, and always brings creative solutions to the table.",
+    name: "Aditya Chatterjee",
+    title: "CEO of BigWig Media & Events Pvt Ltd",
+  },
+  {
+    quote:
+      "Shivang transformed our vision into a fully functional product that exceeded our expectations. His attention to detail and commitment to quality made the entire development process smooth and efficient. I highly recommend him to anyone looking for a reliable and talented developer.",
+    name: "Tushar Jain",
+    title: "Director of Trishla Entertainment",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "App Developer",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Designed and built a responsive website for a client, focusing on user experience and modern web technologies.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer",
+    desc: "Designed and implemented frontend and backend features with modern tech for a seamless user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/shivangbirla",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/ShivangBirla",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/shivang-birla-387254253/",
+  },
+];
