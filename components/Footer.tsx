dc4b16f095063a5a2fc0209e5d1c600a7e4ca699
shivangbirla <birlashivang01@gmail.com>
2024-09-29 23:30:16 +0530

    updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,11 +40,8 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <Link href={info.link} passHref>
-              <div
-                key={info.id}
-                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-              >
+            <Link href={info.link} key={info.id} passHref>
+              <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
                 <img src={info.img} alt="icons" width={20} height={20} />
               </div>
             </Link>
